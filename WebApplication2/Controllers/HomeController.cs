1a794fc64904e0e1ce0c1ea0344f42b0a80e834e
dybichev Yuriy <johndoe@example.com>
2024-12-08 04:44:52 +0700

    Добавьте файлы проекта.

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,190 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private readonly ICalculationService _calculationService;
+        private readonly ILogger<HomeController> _logger;
+
+        
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+           
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        [HttpGet]
+        [HttpPost]
+        public IActionResult ManualParsingSingleAction()
+        {
+            string result = null;
+            double number1 = 0;
+            double number2 = 0;
+            string operation = null;
+
+            if (Request.Method == "POST")
+            {
+                number1 = double.TryParse(Request.Form["Number1"], out var n1) ? n1 : 0;
+                number2 = double.TryParse(Request.Form["Number2"], out var n2) ? n2 : 0;
+                operation = Request.Form["Operation"];
+
+                result = operation switch
+                {
+                    "+" => (number1 + number2).ToString(),
+                    "-" => (number1 - number2).ToString(),
+                    "*" => (number1 * number2).ToString(),
+                    "/" => number2 != 0 ? (number1 / number2).ToString() : "Ошибка: деление на ноль",
+                    _ => "Неизвестная операция"
+
+
+                };
+            }
+            ViewBag.Number1 = number1; // Передача number1 в представление
+            ViewBag.Number2 = number2;
+            ViewBag.Operation = operation;
+            ViewBag.Result = result;
+            ViewData["Result"] = result;
+            return View();
+        }
+
+
+
+        [HttpGet]
+        public IActionResult ManualParsingSeparateAction(int? id = null)
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult ManualParsingSeparateAction()
+        {
+            string result = null;
+            double number1 = 0;
+            double number2 = 0;
+            string operation = null;
+
+            if (Request.Method == "POST")
+            {
+                number1 = double.TryParse(Request.Form["Number1"], out var n1) ? n1 : 0;
+                number2 = double.TryParse(Request.Form["Number2"], out var n2) ? n2 : 0;
+                operation = Request.Form["Operation"];
+
+                result = operation switch
+                {
+                    "+" => (number1 + number2).ToString(),
+                    "-" => (number1 - number2).ToString(),
+                    "*" => (number1 * number2).ToString(),
+                    "/" => number2 != 0 ? (number1 / number2).ToString() : "Ошибка: деление на ноль",
+                    _ => "Неизвестная операция"
+
+
+                };
+            }
+            ViewBag.Number1 = number1; // Передача number1 в представление
+            ViewBag.Number2 = number2;
+            ViewBag.Operation = operation;
+            ViewBag.Result = result;
+            ViewData["Result"] = result;
+            return View();
+        }
+
+
+
+
+
+        [HttpGet]
+        public IActionResult ModelBindingParameters(int? id = null)
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult ModelBindingParameters(double number1, double number2, string operation)
+        {
+            string result = null;
+          
+
+           
+
+                result = operation switch
+                {
+                    "+" => (number1 + number2).ToString(),
+                    "-" => (number1 - number2).ToString(),
+                    "*" => (number1 * number2).ToString(),
+                    "/" => number2 != 0 ? (number1 / number2).ToString() : "Ошибка: деление на ноль",
+                    _ => "Неизвестная операция"
+
+
+                };
+            
+            //ViewBag.Number1 = number1; 
+            //ViewBag.Number2 = number2;
+            //ViewBag.Operation = operation;
+            ViewBag.Result = result;
+            ViewData["Result"] = result;
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult ModelBindingSeparateModel(int? id = null)
+        {
+            return View(new Value());
+        }
+
+
+        [HttpPost]
+        public IActionResult ModelBindingSeparateModel(Value model)
+        {
+            model.Result = null;
+
+
+
+
+            model.Result = model.Operation switch
+            {
+                "+" => (model.Number1 + model.Number2).ToString(),
+                "-" => (model.Number1 - model.Number2).ToString(),
+                "*" => (model.Number1 * model.Number2).ToString(),
+                "/" => model.Number2 != 0 ? (model.Number1 / model.Number2).ToString() : "Ошибка: деление на ноль",
+                _ => "Неизвестная операция"
+
+
+            };
+
+            //ViewBag.Number1 = number1; 
+            //ViewBag.Number2 = number2;
+            //ViewBag.Operation = operation;
+            //ViewBag.Result = result;
+            //ViewData["Result"] = result;
+            return View(model);
+        }
+
+
+
+        
+
+        
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
+
+       
+    }
+}
---
+++ WebApplication2/Models/Value.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication2.Models
+{
+    public class Value
+    {
+        public double Number1 { get; set; }
+        public double Number2 { get; set; }
+        public string Operation { get; set; }
+        public string Result { get; set; }
+    }
+}
